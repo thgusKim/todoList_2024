ff8acf6174553c16ac5b490ae29acf9ed249b725
SoHyeon Kim <thgus7772@naver.com>
2024-02-15 00:39:46 +0900

    second commit

--- main.js
+++ main.js
@@ -2,6 +2,9 @@
 // + 버튼을 클릭하면 할일이 추가된다.
 // delete 버튼을 클릭하면 할일이 삭제된다.
 // check 버튼을 클릭하면 할일이 끝나면서 밑줄이 간다.
+//  1. check 클릭 : true false
+//  2. true면 끝난 걸로 간주하고 밑줄 보여주기
+//  3. false면 안끝난 걸로 간주하고 그대로
 // 진행 중 끝남 탭을 누르면, 언더바가 이동한다.
 // 끝난 탭은 끝난 아이템만, 진행 중은 진행 중인 아이템만 보인다.
 // 전체 탭을 누르면 다시 전체 아이템으로 돌아온다.
@@ -12,24 +15,63 @@ let taskList = [];
 
 addButton.addEventListener("click", addTask);
 
-function addTask(){
-    let taskContent = taskInput.value;
-    taskList.push(taskContent);
+function addTask() {
+    let task = {
+        id: randomIDGenerate(),
+        taskContent: taskInput.value,
+        isComplete: false
+    }
+    taskList.push(task);
     console.log(taskList);
     render();
 }
 
-function render(){
+function render() {
     let resultHTML = '';
-    for(let i=0; i<taskList.length; i++){
-        resultHTML += `<div class="task">
-        <div>${taskList[i]}</div>
+    for (let i = 0; i < taskList.length; i++) {
+        if (taskList[i].isComplete == true) {
+            resultHTML += `<div class="task">
+            <div class="task-done">${taskList[i].taskContent}</div>
+            <div>
+                <button onclick="toggleComplete('${taskList[i].id}')">Check</button>
+                <button onclick="deleteTask('${taskList[i].id}')">Delete</button>
+            </div>
+        </div>`
+        } else {
+            resultHTML += `<div class="task">
+        <div>${taskList[i].taskContent}</div>
         <div>
-            <button>Check</button>
-            <button>Delete</button>
+            <button onclick="toggleComplete('${taskList[i].id}')">Check</button>
+            <button onclick="deleteTask('${taskList[i].id}')">Delete</button>
         </div>
     </div>`
+        }
     }
 
     document.getElementById("task-board").innerHTML = resultHTML;
+}
+
+function toggleComplete(id) {
+    for (let i = 0; i < taskList.length; i++) {
+        if (taskList[i].id == id) {
+            taskList[i].isComplete = !taskList[i].isComplete;
+            break;
+        }
+    }
+    render();
+}
+
+function randomIDGenerate() {
+    return '_' + Math.random().toString(36).substr(2, 9);
+}
+
+function deleteTask(id){
+    for(let i=0; i<taskList.length; i++){
+        if(taskList[i].id == id){
+            taskList.splice(i,1);
+            break;
+        }
+    }
+    render();
+    console.log(taskList);
 }
